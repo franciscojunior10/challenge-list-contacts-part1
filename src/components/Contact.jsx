c65d8e5c9d94f8cf5c2792fe458c2be736c8980e
Francisco Junior <franciscojn49@gmail.com>
2020-05-14 17:07:35 -0300

    feat: applied filters and challenge accepted

---
+++ src/components/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Contact.scss';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <article data-testid="contact" className="contact" key={this.props.data.id}>
+        <span className="contact__avatar">
+          <img src={this.props.data.avatar} alt={this.props.data.name} />
+        </span>
+        <span className="contact__data">{this.props.data.name}</span>
+        <span className="contact__data">{this.props.data.phone}</span>
+        <span className="contact__data">{this.props.data.country}</span>
+        <span className="contact__data">{this.props.data.admissionDate}</span>
+        <span className="contact__data">{this.props.data.company}</span>
+        <span className="contact__data">{this.props.data.department}</span>
+      </article>
+    );
+  }
+}
+
+export default Contact;
