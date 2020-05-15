f0a43e6566aca2ea67d34a04cccc1c498ff2f114
Francisco Junior <franciscojn49@gmail.com>
2020-05-15 20:51:10 -0300

    feat: format date

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 import './Contact.scss';
 
 class Contact extends React.Component {
+  
+  formatDate = (date) => {
+    return new Date(date).toLocaleDateString();
+  }
+
+  
   render() {
     return (
       <article data-testid="contact" className="contact" key={this.props.data.id}>
@@ -12,7 +18,7 @@ class Contact extends React.Component {
         <span className="contact__data">{this.props.data.name}</span>
         <span className="contact__data">{this.props.data.phone}</span>
         <span className="contact__data">{this.props.data.country}</span>
-        <span className="contact__data">{this.props.data.admissionDate}</span>
+        <span className="contact__data">{this.formatDate(this.props.data.admissionDate)}</span>
         <span className="contact__data">{this.props.data.company}</span>
         <span className="contact__data">{this.props.data.department}</span>
       </article>
